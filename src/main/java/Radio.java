a1cf2fb237e325b9e824d18625ca61265a22ab5f
Irinarh13 <158996480+Irinarh13@users.noreply.github.com>
2024-04-01 03:51:02 -0700

    Update Radio.java

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,65 +1,105 @@
 public class Radio {
-    private int currentStation;
-    private int currentSound;
 
+    private int countStation;
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int currentStation = minStation;
+    private int maxSound = 100;
+    private int minSound = 0;
+    private int currentSound = minSound;
+
+    public Radio(int count) {
+        countStation = minStation + count;
+        maxStation = countStation - 1;
+        minStation = 0;
+    }
+
+    public Radio() {
+        countStation = 10;
+    }
+
+    public int getCountStation() {
+        return countStation;
+    }
+    public int getMaxStation() {
+        return maxStation;
+    }
+    public int getMinStation() {
+        return minStation;
+    }
     public int getCurrentStation() {
         return currentStation;
     }
-
     public int getCurrentSound() {
         return currentSound;
     }
 
     public void setCurrentStation(int newCurrentStation) {
-        if (newCurrentStation < 0) {
+        if (newCurrentStation > maxStation) {
+            setCurrentStation(minStation);
             return;
         }
-        if (newCurrentStation > 9) {
+        if (newCurrentStation < minStation) {
+            setCurrentStation(maxStation);
             return;
         }
         currentStation = newCurrentStation;
     }
 
     public void setCurrentSound(int newCurrentSound) {
-        if (newCurrentSound < 0) {
+        if (newCurrentSound < minSound) {
+            setCurrentSound(minSound);
             return;
         }
-        if (newCurrentSound > 100) {
+        if (newCurrentSound > maxSound) {
+            setCurrentSound(maxSound);
             return;
         }
         currentSound = newCurrentSound;
     }
 
-    public void setNextStationAfterMax() {
-        if (currentStation < 9) {
-            currentStation = currentStation + 1;
-        } else {
-            currentStation = 0;
+    public void next() {
+        int nextStation = currentStation + 1;
+        if (nextStation < maxStation) {
+            setCurrentStation(nextStation);
         }
+        if (nextStation > maxStation) {
+            setCurrentStation(minStation);
+        }
+        return;
     }
 
-    public void setPrevStation() {
-        if (currentStation > 0) {
-            currentStation = currentStation - 1;
-        } else {
-            currentStation = 9;
+    public void prev() {
+        int prevStation = currentStation - 1;
+        if (prevStation > minStation) {
+            setCurrentStation(prevStation);
         }
+        if (prevStation < minStation) {
+            setCurrentStation(maxStation);
+        }
+        return;
     }
 
-    public void setIncreaseSound() {
-        if (currentSound < 100) {
-            currentSound = currentSound + 1;
-        } else {
-            currentSound = 100;
+    public void increaseSound() {
+        int nextSound = currentSound + 1;
+        if (currentSound == maxSound) {
+            setCurrentSound(maxSound);
         }
+        if (currentSound < maxSound) {
+            setCurrentSound(nextSound);
+        }
+        return;
     }
 
-    public void setDecreaseSound() {
-        if (currentSound > 0) {
-            currentSound = currentSound - 1;
-        } else {
-            currentSound = 0;
+    public void decreaseSound() {
+        int prevSound = currentSound - 1;
+        if (currentSound <= minSound) {
+            setCurrentSound(minSound);
         }
+        if (currentSound > minSound) {
+            setCurrentSound(prevSound);
+        }
+        return;
     }
+
 }
-
